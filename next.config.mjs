037ed404e190d15d124fdab2553528dbb0075210
Aufafikri <fikriilmi6@gmail.com>
2024-07-23 23:55:55 +0700

    final deployments

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
+                // protocol: "http",
                 hostname: "image.tmdb.org"
             }
         ]
